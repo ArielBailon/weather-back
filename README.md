9e9e2b2858d04be63d7688ad1b7d32c119e6da9f
abailonn <abailon@sorti365.com.ec>
2023-03-14 16:52:52 -0500

    final touches

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# weather-back
+# Weather APP - Back
+This weather app allows you to fetch and see the day's weather by the location name, it also allows you to get the forecast for the next 5 days. The project connects and delivers data from to the OpenWeather API by using the npm `openweather-api-node` package. The project was done in `Node.js version 16`. 
+
+
+## Installation
+Clone the project by using:
+```sh
+git clone https://github.com/ArielBailon/weather-back
+```
+ After that you'll need to install the dependencies by using:
+ ```sh
+npm install
+```
+ Before you can start the project there's a file called `.env.sample` which you should create a copy with the name `.env`. For the project to run succesfully you'd have to fill the params in this file, the `WEATHER_API_KEY` param can be obtained on the website: `https://openweathermap.org/`.
+ 
+ Once that is done, run the script:
+ ```
+ npm run start
+ ```
+ 
+ This will make run the script located at the `package.json` file, after that the project will run on the port `3000` and you can access it by going to `http://localhost:3000`
+
+If you followed the steps you should be able to use this API without any problem.
--- controllers/weatherController.js
+++ controllers/weatherController.js
@@ -3,22 +3,34 @@ const { OpenWeatherAPI } = require("openweather-api-node");
 const dotenv = require('dotenv').config();
 const { success, error } = require('../responseApi/responseApiFront')
 
+// Sets instance of OpenWeatherAPI
+// f1f3333e6742de7ed86081f0fd1959e3
 let weather = new OpenWeatherAPI({
   key: process.env.WEATHER_API_KEY,
   units: "imperial"
 })
+
+// Cleans forecast data to have one forecast per day
+function dataByDay(data){
+  let arrDataByDay = [];
+
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].dt.getDay() !== data[i+1]?.dt.getDay()) {
+      arrDataByDay.push(data[i]);
+    }
+  }
+  arrDataByDay.pop();
+  return arrDataByDay;
+}
 
 // @desc    Gets today's weather
 // @route   POST /api/weather/getTodaysWeather
 // @access  Public
 const getTodaysWeather = asyncHandler(async (req, res) => {
   weather.setLocationByName(req.body.locationName)
-  console.log(weather)
 
   weather.getCurrent().then(data => {
-    console.log(data)
     res.status(200).json(success(200, data))
-    // console.log(`Current location is: ${data.weather.temp}\u00B0F`)
   }).catch((err)=> {
     res.status(400).json(error(400, err.message))
   })
@@ -29,16 +41,14 @@ const getTodaysWeather = asyncHandler(async (req, res) => {
 // @access  Public
 const getForecast = asyncHandler(async (req, res) => {
   weather.setLocationByName(req.body.locationName)
-  console.log(weather)
 
   weather.getForecast().then(data => {
-    console.log(data)
-    res.status(200).json(success(200, data))
-    // console.log(`Current location is: ${data.weather.temp}\u00B0F`)
+    let cleanData = dataByDay(data);
+    res.status(200).json(success(200, cleanData))
   }).catch((err)=> {
-    res.status(400).json(error(400, err))
+    console.log(err);
+    res.status(400).json(error(400, err.message))
   })
 })
-
 
 module.exports = { getTodaysWeather, getForecast }
